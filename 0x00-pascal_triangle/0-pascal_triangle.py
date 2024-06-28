7ba97a01ab0521ccf4d10fe31ecdb452716752cc
Sohail Charef <charefsohayl@gmail.com>
2024-06-28 04:06:52 -0700

    pycodestyle fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,15 @@
 
 
 def pascal_triangle(n):
+    """
+    Generates the Pascal's triangle up to the n-th row.
+
+    Args:
+        n (int): The number of rows to generate in the Pascal's triangle.
+
+    Returns:
+        list: A list containing the rows of the Pascal's triangle.
+    """
     if n <= 0:
         return
     Triangle = [[1]]
@@ -15,6 +24,15 @@ def pascal_triangle(n):
 
 
 def next_row(current_row):
+    """
+    Generates the next row of the Pascal's triangle from the current row.
+
+    Args:
+        current_row (list): The current row of the Pascal's triangle.
+
+    Returns:
+        list: The next row of the Pascal's triangle.
+    """
     new_row = [1]
     for i in range(len(current_row) - 1):
         new_row.append(current_row[i] + current_row[i+1])
