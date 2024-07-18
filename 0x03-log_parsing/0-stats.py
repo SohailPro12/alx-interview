0435b99f4fab20290162fe6074126ed59affca7e
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-18 13:43:27 +0200

    log parsing interview question done

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Log parsing"""
+
+import sys
+import re
+import signal
+
+pattern = r'(\d{3}) (\d+)$'
+status_counts = {}
+total_file_size = 0
+line_count = 0
+
+
+def handle_interrupt(signal, frame):
+    print(f"File size: {total_file_size}")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, handle_interrupt)
+
+for line in sys.stdin:
+    line_count += 1
+    match = re.search(pattern, line)
+    if match:
+        status_code = int(match.group(1))
+        file_size = int(match.group(2))
+        status_counts[status_code] = status_counts.get(status_code, 0) + 1
+        total_file_size += file_size
+        if line_count % 10 == 0:
+            print(f"File size: {total_file_size}")
+            i = 0
+        else:
+            print(status_code, status_counts[status_code])
