8133d21f3a730fe931fe18577f27cb6e46273df0
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-07-18 13:57:55 +0200

    log parsing interview question done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,8 @@ import re
 import signal
 
 pattern = r'(\d{3}) (\d+)$'
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
+                 403: 0, 404: 0, 405: 0, 500: 0}
 total_file_size = 0
 line_count = 0
 
