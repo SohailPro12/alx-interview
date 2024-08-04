ee11690d0a214b5d2cd0d26a586432d25b27d3ef
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-08-04 12:50:21 +0200

    refactor: improve UTF-8 validation logic

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,26 @@ represents a valid UTF-8 encoding.
 
 
 def validUTF8(data):
-    for x in data:
-        if x > 127 or x < 0:
-            return False
-    return True
+    num_bytes = 0
+    mask1 = 1 << 7  # 10000000 in binary
+    mask2 = 1 << 6  # 01000000 in binary
+
+    for byte in data:
+        byte = byte & 0xFF
+        if num_bytes == 0:
+            mask = 1 << 7
+            while (byte & mask):
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
