a0a902b6ab67d8c6235a8d849d676579e58b5342
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-09-03 15:45:08 +0200

    n queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,22 +3,72 @@
 N Queens problem
 """
 
+import sys
+
+
+def print_solution(board):
+    """Prints a board solution in the required format."""
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    """Checks if it's safe to place a queen at board[row][col]."""
+    # Check the current row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check the upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check the lower diagonal on the left side
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, col):
+    """Uses backtracking to find all solutions for the N Queens problem."""
+    if col >= len(board):
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+
+    return res
+
+
+def nqueens(N):
+    """Initializes the board and starts solving the N Queens problem."""
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
-    """
-    N Queens problem
-    
-    """
-    import sys
-    n = sys.argv[1]
     if len(sys.argv) != 2:
-        print("Usage: python n_queens.py N")
+        print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except Exception:
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+
+    nqueens(n)
